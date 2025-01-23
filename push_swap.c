0193924de00316e24f09e377fd0fc81ae2ad87c3
Shiyan Guan <sguan@paul-f4Ar8s6.clusters.42paris.fr>
2025-01-23 23:10:13 +0100

    regular backup

---
+++ push_swap.c
@@ -0,0 +1,51 @@
+#include "push_swap.h"
+
+t_node    *get_stack(char **arr)
+{
+    int i;
+    t_node *head = NULL;
+    t_node *current = NULL;
+
+    i = 1;
+    while (arr[i])
+    {
+        t_node *node = malloc(sizeof(t_node));  
+        node->value = atoi(arr[i]);
+
+        if (head == NULL)
+        {
+            head = node;
+            current = node;
+        }
+        else
+        {
+            current->next = node; 
+            current = node;
+        }    
+        i++;
+    }
+    return (head);
+}
+
+
+#include <stdio.h> //TO REMOVE
+
+int main(int argc, char **argv)
+{
+    if (argc == 1)
+        return (0);
+    t_node *head_a;
+    t_node *current_a;
+    t_node *head_b;
+
+    head_a = get_stack(argv);
+    current_a = head_a;
+
+    while (current_a != NULL)
+    {
+        printf("%d\n",current_a->value);
+        current_a = current_a->next;
+    }
+
+    head_b = NULL;
+}
---
+++ push_swap.h
@@ -0,0 +1,11 @@
+#ifndef H_PUSH
+# define H_push
+
+#include <stdlib.h>
+
+typedef struct s_node{
+    int value;
+    struct s_node *next;
+} t_node;
+
+#endif
